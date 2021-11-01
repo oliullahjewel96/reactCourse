d1aa349061ce0c86e536da4c51eb9097b728993c
Oli Ullah Jewel <oli.jewel@northsouth.edu>
2021-11-01 17:48:51 +0600

    Created first class component in React

--- contactmanager/src/App.js
+++ contactmanager/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import Contact from "./components/Contact";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>The App Component</h1>
+      <Contact />
     </div>
   );
 }
