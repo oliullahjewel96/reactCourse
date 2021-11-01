b20ef1c2bfaa5155b526bbd36100f60d1559b29c
Oli Ullah Jewel <oli.jewel@northsouth.edu>
2021-11-01 18:06:52 +0600

    Created first functional component

--- contactmanager/src/App.js
+++ contactmanager/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import Contact from "./components/Contact";
+import Header from "./components/Header";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>The App Component</h1>
+      <Header />
       <Contact />
     </div>
   );
