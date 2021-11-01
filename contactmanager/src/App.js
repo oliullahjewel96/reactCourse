0abe700f32a1fb7cee6c7fd885261e6110d97ae4
Oli Ullah Jewel <oli.jewel@northsouth.edu>
2021-11-01 21:03:07 +0600

    Adding Bootstap in React

--- contactmanager/src/App.js
+++ contactmanager/src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
 import Contact from "./components/Contact";
 import Header from "./components/Header";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Header branding="Contact manager" />
-      <Contact name="John" email="john@gamil.com" phone="555-555-5555" />
-      <Contact name="oli Ullah" email="oli@gamil.com" phone="01860594161" />
+      <div className="container">
+        <Contact name="John" email="john@gamil.com" phone="555-555-5555" />
+        <Contact name="oli Ullah" email="oli@gamil.com" phone="01860594161" />
+      </div>
     </div>
   );
 }
