79b327b904cafc2375097b718c59b9c3a69ebe91
Oli Ullah Jewel <oli.jewel@northsouth.edu>
2021-11-02 15:44:48 +0600

    Changing state from another components

--- contactmanager/src/components/Contact.js
+++ contactmanager/src/components/Contact.js
@@ -17,6 +17,9 @@ class Contact extends Component {
   //     showStateInfo: !this.state.showStateInfo,
   //   });
   // };
+  onClickDelete = () => {
+    this.props.deleteClickHandler();
+  };
   render() {
     const { name, email, phone } = this.props.contact;
     const { showStateInfo } = this.state;
@@ -31,6 +34,12 @@ class Contact extends Component {
               })
             }
             className="fas fa-sort-down"
+            style={{ cursor: "pointer" }}
+          ></i>
+          <i
+            className="fas fa-times"
+            style={{ cursor: "pointer", float: "right", color: "red" }}
+            onClick={this.onClickDelete}
           ></i>
         </h4>
         {showStateInfo ? (
@@ -45,9 +54,8 @@ class Contact extends Component {
 }
 
 Contact.propTypes = {
-  name: PropTypes.string.isRequired,
-  email: PropTypes.string.isRequired,
-  phone: PropTypes.string.isRequired,
+  contact: PropTypes.object.isRequired,
+  deleteClickHandler: PropTypes.func.isRequired,
 };
 
 export default Contact;
