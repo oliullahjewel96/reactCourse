c308f991d91af8705032ff0ec0e5e12772686e16
Oli Ullah Jewel <oli.jewel@northsouth.edu>
2021-11-02 12:37:49 +0600

    Events in React

--- contactmanager/src/components/Contact.js
+++ contactmanager/src/components/Contact.js
@@ -7,11 +7,23 @@ class Contact extends Component {
   //     email: PropTypes.string.isRequired,
   //     phone: PropTypes.string.isRequired,
   //   };
+
+  state = {};
+
+  onShowClick = (name, e) => {
+    console.log(name);
+  };
   render() {
     const { name, email, phone } = this.props.contact;
     return (
       <div className="card card-body mb-3">
-        <h4>{name}</h4>
+        <h4>
+          {name}
+          <i
+            onClick={this.onShowClick.bind(this, name)}
+            className="fas fa-sort-down"
+          ></i>
+        </h4>
         <ul className="list-group">
           <li className="list-group-item">Email:{email}</li>
           <li className="list-group-item">Phone: {phone}</li>
